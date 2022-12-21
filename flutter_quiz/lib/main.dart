325ce427986e05d475d8c4249ab3017dceab66df
Shubh0405 <shubhnaresh.gupta2019@vitstudent.ac.in>
2022-12-22 00:26:07 +0530

    init code

--- flutter_quiz/lib/main.dart
+++ flutter_quiz/lib/main.dart
@@ -4,22 +4,39 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    var questions = [
+      'Where is FIFA 2022 world cup hosted?',
+      'Which country won the FIFA world cup 2018?'
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Quiz App'),
         ),
         body: Column(
-          children: const [
-            Text('Answer the following question!'),
-            ElevatedButton(onPressed: null, child: Text('Answer 1')),
-            ElevatedButton(onPressed: null, child: Text('Answer 2')),
-            ElevatedButton(onPressed: null, child: Text('Answer 3'))
+          children: [
+            Text(questions[questionIndex]),
+            ElevatedButton(onPressed: answerQuestion, child: Text('Answer 1')),
+            ElevatedButton(onPressed: answerQuestion, child: Text('Answer 2')),
+            ElevatedButton(onPressed: answerQuestion, child: Text('Answer 3')),
           ],
         ),
       ),
